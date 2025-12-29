6c79952890e9a4ae6d83795f37bbb2a73c939285
Al Zaplin <zaplin.dev@gmail.com>
2025-12-29 20:26:59 +0300

    Feat: (Get data from server) With fetch and save it into state to render on the page

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,28 @@ import '@/assets/fonts/proxima-nova-bold.ttf'
 
 import '@/scss/app.scss'
 
-import { assortment } from '@/db/db.json'
+// import { assortment } from '@/db/db.json'
 
 import Header from "@/components/Header.jsx"
 import Categories from "@/components/Categories.jsx"
 import Sort from "@/components/Sort.jsx"
 import PizzaItem from "@/components/PizzaItem.jsx"
+import React from "react";
+
 
 export default function App() {
+  // 6.1 Отлично, когда мы удостоверились, что данные с сервера успешно получаются, то мы можем поместить их в стейт.
+  const [items, setItems] = React.useState([])
+
+  // * 6.0 Так как получать данные из локального JSON-файла в виде БД в реальных проектах не принято, то сделаем это через тренировочное API "MockAPI" при помощи метода fetch. Через метод "then", после ответа сервера мы получаем этот ответ и переводим его в вид, который сможем использовать в приложении — JSON методом "json".
+  // 6.2 Мы также используем метод записи в стейт "setItems", чтоб динамически обновлять стейт свежими полученными с сервера данными, куда и поместим их.
+  // 6.3 Однако непозволительно просто так вызывать метод fetch, так как это приведёт к проблеме infinity loop, когда метод "fetch" будет обновлять стейт, тем самым заново запуская функцию "App", таким образов снова запуская fetch и так, пока сервер не заблокирует нас или не зависнет браузер. Чтобы fetch, или запрос на сервер выполнялся лишь раз, разумно будет использовать хук "useEffect".
+  // ? Кстати, когда у нас вторым аргументом в "useEffect" пустой массив, то это значит выполнить функцию лишь раз при первом рендере компонента, а если поместить в этот массив какой-нибудь стейт, то функция будет следить за изменениями его значения и запускать функцию вновь, если значение изменится.
+  React.useEffect(() => {
+      fetch('https://6952a95b3b3c518fca135a9c.mockapi.io/items')
+        .then(response => response.json())
+        .then((data) => setItems(data))
+    }, [])
 
 	return (
 		<div className="wrapper">
@@ -32,13 +46,13 @@ export default function App() {
 			<div className="content">
 				<div className="container">
 					<div className="content__top">
-						{/* <Categories /> */}
+						 <Categories />
 						<Sort />
 					</div>
 					<h2 className="content__title">Все пиццы</h2>
 					<div className="content__items">
 						{
-							assortment.map(item => (
+							items.map(item => (
 								/*<PizzaItem key={item.id} img={item.img} title={item.title} filling={item.filling} doughTypes={item.doughTypes} sizes={item.sizes} price={item.price}/>
 								* Если мы видим, что все свойства объекта совпадают с названием пропов, то мы можем существенно сократить: */
 								<PizzaItem key={item.title} {...item} />
