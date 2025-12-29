39bc1cacebec431222277a771fff1e13aaa21991
Al Zaplin <zaplin.dev@gmail.com>
2025-12-29 14:53:08 +0300

    Feat: (Sorting Pizzas) UI: Activate popup for choosing sorting type

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,53 @@
-import './assets/fonts/proxima-nova-regular.eot'
-import './assets/fonts/proxima-nova-regular.woff2'
-import './assets/fonts/proxima-nova-regular.woff'
-import './assets/fonts/proxima-nova-regular.ttf'
-import './assets/fonts/proxima-nova-extrabold.eot'
-import './assets/fonts/proxima-nova-extrabold.woff2'
-import './assets/fonts/proxima-nova-extrabold.woff'
-import './assets/fonts/proxima-nova-extrabold.ttf'
-import './assets/fonts/proxima-nova-black.eot'
-import './assets/fonts/proxima-nova-black.woff2'
-import './assets/fonts/proxima-nova-black.woff'
-import './assets/fonts/proxima-nova-black.ttf'
-import './assets/fonts/proxima-nova-bold.eot'
-import './assets/fonts/proxima-nova-bold.woff2'
-import './assets/fonts/proxima-nova-bold.woff'
-import './assets/fonts/proxima-nova-bold.ttf'
+import '@/assets/fonts/proxima-nova-regular.eot'
+import '@/assets/fonts/proxima-nova-regular.woff2'
+import '@/assets/fonts/proxima-nova-regular.woff'
+import '@/assets/fonts/proxima-nova-regular.ttf'
+import '@/assets/fonts/proxima-nova-extrabold.eot'
+import '@/assets/fonts/proxima-nova-extrabold.woff2'
+import '@/assets/fonts/proxima-nova-extrabold.woff'
+import '@/assets/fonts/proxima-nova-extrabold.ttf'
+import '@/assets/fonts/proxima-nova-black.eot'
+import '@/assets/fonts/proxima-nova-black.woff2'
+import '@/assets/fonts/proxima-nova-black.woff'
+import '@/assets/fonts/proxima-nova-black.ttf'
+import '@/assets/fonts/proxima-nova-bold.eot'
+import '@/assets/fonts/proxima-nova-bold.woff2'
+import '@/assets/fonts/proxima-nova-bold.woff'
+import '@/assets/fonts/proxima-nova-bold.ttf'
 
-import './scss/app.scss'
+import '@/scss/app.scss'
 
-import {assortment} from '../db.json'
+import { assortment } from '@/db/db.json'
 
-import Header from "./components/Header.jsx";
-import Categories from "./components/Categories.jsx";
-import Sort from "./components/Sort.jsx";
-import PizzaItem from "./components/PizzaItem.jsx";
+import Header from "@/components/Header.jsx"
+import Categories from "@/components/Categories.jsx"
+import Sort from "@/components/Sort.jsx"
+import PizzaItem from "@/components/PizzaItem.jsx"
 
 export default function App() {
 
-  return (
-    <div className="wrapper">
-      <Header/>
-      <div className="content">
-        <div className="container">
-          <div className="content__top">
-            <Categories/>
-            <Sort/>
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {
-              assortment.map(item => (
-                /*<PizzaItem key={item.id} img={item.img} title={item.title} filling={item.filling} doughTypes={item.doughTypes} sizes={item.sizes} price={item.price}/>
-                * Если мы видим, что все свойства объекта совпадают с названием пропов, то мы можем существенно сократить: */
-              <PizzaItem key={item.title} {...item}/>
-              ))
-            }
-            {/*<PizzaItem img="./src/assets/img/pizza/cola-bbq.webp" title="Кола-барбекю" filling="Пряная говядина, пикантная пепперони, острые колбаски чоризо, соус кола-барбекю, моцарелла и фирменный томатный соус" price={599}/>*/}
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+	return (
+		<div className="wrapper">
+			<Header />
+			<div className="content">
+				<div className="container">
+					<div className="content__top">
+						{/* <Categories /> */}
+						<Sort />
+					</div>
+					<h2 className="content__title">Все пиццы</h2>
+					<div className="content__items">
+						{
+							assortment.map(item => (
+								/*<PizzaItem key={item.id} img={item.img} title={item.title} filling={item.filling} doughTypes={item.doughTypes} sizes={item.sizes} price={item.price}/>
+								* Если мы видим, что все свойства объекта совпадают с названием пропов, то мы можем существенно сократить: */
+								<PizzaItem key={item.title} {...item} />
+							))
+						}
+						{/*<PizzaItem img="./src/assets/img/pizza/cola-bbq.webp" title="Кола-барбекю" filling="Пряная говядина, пикантная пепперони, острые колбаски чоризо, соус кола-барбекю, моцарелла и фирменный томатный соус" price={599}/>*/}
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
