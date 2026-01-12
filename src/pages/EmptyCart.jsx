6b6b355c3fbc7d398e9c387c44054c357c3d0c0b
Al Zaplin <zaplin.dev@gmail.com>
2026-01-12 08:43:54 +0300

    Small fixes

--- src/pages/EmptyCart.jsx
+++ src/pages/EmptyCart.jsx
@@ -1,15 +1,15 @@
 import {Link} from "react-router";
 
-export const EmptyCart = () => {
+const EmptyCart = () => {
   return (
     <div className="container container--cart">
       <div className="cart cart--empty">
-        <h2>Корзина пуста <icon>😕</icon></h2>
+        <h2>Корзина пуста 😕</h2>
         <p>
           Вероятно, вы ещё не добавили пиццу в корзину.<br/>
           Пожалуйста, перейдите на главную страницу, чтобы выбрать и добавить пиццу в корзину.
         </p>
-        <img src="/img/empty-cart.png" alt="Empty cart"/>
+        <img src="/src/assets/img/empty-cart.png" alt="Empty cart"/>
         <Link to="/" className="button button--black">
           <span>Вернуться на главную страницу</span>
         </Link>
@@ -17,3 +17,5 @@ export const EmptyCart = () => {
     </div>
   )
 }
+
+export default EmptyCart
