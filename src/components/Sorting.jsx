9d345e27b89300f4702738c2395b5245bf3aa366
Al Zaplin <zaplin.dev@gmail.com>
2026-01-18 15:34:31 +0300

    Feat:(Sorting pop-up) Close the pop-up by clicking outside of it.

--- src/components/Sorting.jsx
+++ src/components/Sorting.jsx
@@ -3,48 +3,61 @@ import {useDispatch, useSelector} from "react-redux"
 import {setSortingDirection, setSortingTypeId} from "/src/redux/slices/filterSlice.js"
 
 const sortingList = [
-    {id: 0, name: 'популярности'},
-    {id: 1, name: 'цене'},
-    {id: 2, name: 'алфавиту'}
-  ]
+  {id: 0, name: 'популярности'},
+  {id: 1, name: 'цене'},
+  {id: 2, name: 'алфавиту'}
+]
 
-// 13.4.0 Здесь же нам нужно переписать код под работу с Redux.
-// function Sorting({value, valueBtn, onChangeSorting, onClickBtn}) {
 function Sorting() {
-  // 13.4.1 Итак, мы получаем в константы данные из хранилища Redux при помощи хука "useSelector". Он работает похожим образом, как в обычном JS EventListener. Т.е. он "слушает" стейт и при его обновлении ререндерит тот компонент, в котором он написан.
-  // ? 13.5 Кстати, мы можем ещё немного сократить код и деструктурированием вытащить стейты в одну строчку.
-  // const sortingId = useSelector((state) => state.filter.sortingId)
-  // const sortingBtnValue = useSelector((state) => state.filter.sortingBtnValue)
   const {sortingTypeId, sortingDirection} = useSelector((state) => state.filter)
   const dispatch = useDispatch()
 
   const [show, setShowing] = React.useState(false)
 
-  const selectSorting = (id) => {
-    // onChangeSorting(id)
-    // 13.4.2 Ну, а с помощью функции "dispatch" мы записываем в стейт слайса данные функцией-редьюсером, которая экспортируется из этого слайса.
+  // 16.0.0 Чтобы реализовать логику скрытия попапа по клику вне его области, мы сперва создадим реф-переменную, куда поместим этот попап.
+  const sortRef = React.useRef(null)
+
+  const selectSorting = id => {
     dispatch(setSortingTypeId(id))
     setShowing(false)
   }
 
-  const dynamicPathToIcon = (direction) => {
+  const dynamicPathToIcon = direction => {
     let iconName;
-    if (direction === 'asc') {
-      sortingTypeId !== 2 ? iconName = 'ascending-sorting.png' : iconName = 'a-z-sorting.png'
-    } else {
+    direction === 'asc' ?
+      sortingTypeId !== 2 ? iconName = 'ascending-sorting.png' : iconName = 'a-z-sorting.png' :
       sortingTypeId !== 2 ? iconName = 'descending-sorting.png' : iconName = 'z-a-sorting.png'
-    }
+
     return iconName
   }
 
-  // const handleSortingBtn = () => onClickBtn(valueBtn === 'asc' ? 'desc' : 'asc')
   const handleSortingBtn = () => dispatch(setSortingDirection(sortingDirection === 'asc' ? 'desc' : 'asc'))
 
+  // 16.0.2 Далее нам нужно создать слушатель события клик по всему элементу body. Это позволит нам определить, был ли клик по области попапа или нет. Здесь у нас будет хук useEffect, который должен вызваться лишь раз при первом рендере компонента. И, в отличие от остальных элементов страницы, где мы не можем просто, как в обычном JS, внутри ReactJS обращаться к элементам, а сначала кладём их сперва в реф-переменные через хук useRef, то сущность body — это исключение, и здесь мы так сделать можем.
+  // 16.0.3 Итак, когда мы кликаем на что-то, то получаем в объект "evt", точнее в его свойство "path" список дочерних элементов, которые содержатся под мышкой в момент клика.
+  React.useEffect(() => {
+    const handleClickOutside = (evt) => {
+      const isOutside = sortRef.current && !sortRef.current.contains(evt.target)
+      const isSortBtn = evt.target.closest('.sort__btn')
+
+      if (isOutside || isSortBtn) setShowing(false)
+    }
+
+    document.body.addEventListener('click', handleClickOutside)
+    // document.body.addEventListener('touchstart', handleClickOutside)
+
+    // Конструкция "return () => {...}" означает выполнение кода перед тем, как элемент уберётся (размонтируется).
+    return () => {
+      document.body.removeEventListener('click', handleClickOutside)
+      // document.body.removeEventListener('touchstart', handleClickOutside)
+    }
+  }, [])
+
+  // 16.0.1 Привяжем её к попапу с помощью директивы "ref".
   return (
-    <div className="sort">
+    <div ref={sortRef} className="sort">
       <div className="sort__label">
         <button onClick={() => handleSortingBtn()} className="sort__btn">
-          {/*<img src={`/src/assets/img/${dynamicPathToIcon(valueBtn)}`} alt=""/>*/}
           <img src={`/src/assets/img/${dynamicPathToIcon(sortingDirection)}`} alt=""/>
         </button>
         <b>Сортировка по:</b>
